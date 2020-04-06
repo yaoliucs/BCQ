5499dd02f9a3129aa89813c001a785842c379b58
yaoliucs <yao.liu.chn@gmail.com>
2020-04-06 01:13:29 -0700

    add noisy evaluation and load buffer size

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def interact_with_environment(env, state_dim, action_dim, max_action, device, ar
 	replay_buffer = utils.ReplayBuffer(state_dim, action_dim, device)
 	
 	evaluations = []
+	episode_values = []
 
 	state, done = env.reset(), False
 	episode_reward = 0
@@ -63,6 +64,7 @@ def interact_with_environment(env, state_dim, action_dim, max_action, device, ar
 		if done: 
 			# +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
 			print(f"Total T: {t+1} Episode Num: {episode_num+1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
+			episode_values.append(episode_reward)
 			# Reset environment
 			state, done = env.reset(), False
 			episode_reward = 0
@@ -79,15 +81,16 @@ def interact_with_environment(env, state_dim, action_dim, max_action, device, ar
 	if args.train_behavioral:
 		policy.save(f"./models/behavioral_{setting}")
 		replay_buffer.save(f"./buffers/{buffer_name}")
-		avg_reward = (replay_buffer.reward.sum()) / (1. - replay_buffer.not_done).sum()
-		np.save(f"./results/buffer_noisy_performance_{setting}", avg_reward)
+		noisy_evaluation = np.mean(episode_values)
+		np.save(f"./results/buffer_average_performance_{setting}", noisy_evaluation)
 
 	# Save final buffer and performance
 	else:
-		evaluations.append(eval_policy(policy, args.env, args.seed))
-		np.save(f"./results/buffer_nonoise_performance_{setting}", evaluations)
-		avg_reward = (replay_buffer.reward.sum())/(1.-replay_buffer.not_done).sum()
-		np.save(f"./results/buffer_noisy_performance_{setting}", avg_reward)
+		evaluations.append(eval_policy(policy, args.env, args.seed, 100))
+		np.save(f"./results/buffer_policy_performance_{setting}", evaluations)
+		noisy_evaluation=eval_noisy_policy(policy, args.env, args.seed,
+										   args.rand_action_p, args.gaussian_std, action_dim, max_action)
+		np.save(f"./results/buffer_average_performance_{setting}", noisy_evaluation)
 		replay_buffer.save(f"./buffers/{buffer_name}")
 
 
@@ -102,7 +105,7 @@ def train_BCQ(state_dim, action_dim, max_action, device, args):
 
 	# Load buffer
 	replay_buffer = utils.ReplayBuffer(state_dim, action_dim, device)
-	replay_buffer.load(f"./buffers/{buffer_name}")
+	replay_buffer.load(f"./buffers/{buffer_name}", args.load_buffer_size)
 	
 	evaluations = []
 	episode_num = 0
@@ -113,7 +116,7 @@ def train_BCQ(state_dim, action_dim, max_action, device, args):
 		pol_vals = policy.train(replay_buffer, iterations=int(args.eval_freq), batch_size=args.batch_size)
 
 		evaluations.append(eval_policy(policy, args.env, args.seed))
-		np.save(f"./results/BCQ_{setting}", evaluations)
+		np.save(f"./results/BCQ_N{args.load_buffer_size}_{setting}", evaluations)
 
 		training_iters += args.eval_freq
 		print(f"Training iterations: {training_iters}")
@@ -206,6 +209,32 @@ def eval_policy(policy, env_name, seed, eval_episodes=10):
 	print("---------------------------------------")
 	return avg_reward
 
+def eval_noisy_policy(policy, env_name, seed, rand_action_p, gaussian_std,
+					  action_dim, max_action, eval_episodes=100):
+	eval_env = gym.make(env_name)
+	eval_env.seed(seed + 100)
+
+	avg_reward = 0.
+	for _ in range(eval_episodes):
+		state, done = eval_env.reset(), False
+		while not done:
+			if (np.random.uniform(0, 1) < rand_action_p):
+				action = env.action_space.sample()
+			else:
+				action = (
+					policy.select_action(np.array(state))
+					+ np.random.normal(0, max_action * gaussian_std, size=action_dim)
+				).clip(-max_action, max_action)
+			state, reward, done, _ = eval_env.step(action)
+			avg_reward += reward
+
+	avg_reward /= eval_episodes
+
+	print("---------------------------------------")
+	print(f"Evaluation policy with noise over {eval_episodes} episodes: {avg_reward:.3f}")
+	print("---------------------------------------")
+	return avg_reward
+
 
 if __name__ == "__main__":
 	
@@ -231,6 +260,7 @@ if __name__ == "__main__":
 	parser.add_argument("--beta_a", default=0.0, type=float)		# state filter hyperparameter (actor)
 	parser.add_argument("--beta_c", default=-2.0, type=float)		# state filter hyperparameter (critic)
 	parser.add_argument("--sigmoid_k", default=100, type=float)
+	parser.add_argument("--load_buffer_size", default=1e6, type=int) # number of samples to load into the buffer
 	args = parser.parse_args()
 
 	print("---------------------------------------")	
