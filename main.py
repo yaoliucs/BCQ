0f7651a2f9d52a5b1ccf8a6133b561a7ebfab61e
yaoliucs <yao.liu.chn@gmail.com>
2020-04-10 14:15:25 -0700

    try fix mojuco warning

--- main.py
+++ main.py
@@ -286,7 +286,7 @@ def eval_policy(policy, env_name, seed, eval_episodes=10):
 		state, done = eval_env.reset(), False
 		while not done:
 			action = policy.select_action(np.array(state))
-			time.sleep(0.001)
+			time.sleep(0.002)
 			state, reward, done, _ = eval_env.step(action)
 			avg_reward += reward
 
